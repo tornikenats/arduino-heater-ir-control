0d33323a5e0f8dcda2fd215dbd3f36446eb43eb5
Tornike Natsvlishvili <1811195+tornikenats@users.noreply.github.com>
2020-07-19 21:45:08 -0400

    Add new code

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# arduino-heater-ir-control
-Use a thermometer and IR LED to turn on and off an electric heater based on temperature
+# Arduino Heater IR Control
+
+Use a thermometer and IR LED to turn on and off an electric heater based on temperature.
+
+## Parts used
+
+1. OLED Screen - SSD1306
+2. Thermometer - DH22
+3. IR LED - 5mm 940nm IR
+
+
+## Code structure
+
+Here is how it works: 
+
+Every second, we check the temperature. If the temperature is below our set temperature, we turn on the heater. 
+
+After the temperature exceed our set temperature, we turn off the heater and go into "cooldown mode". When in this mode we don't turn on the heater if we go below our set temperature. Instead, we wait until it goes a set threshold below and then turn it on again. We do this so we aren't repeatedly turning off and on our heater.
+
+Here is an example:
+
+* We set out target temperature to 22*C and our threshold at 1*C
+* Room is 20*C
+* Heater turns on since we are below 22*C
+* Room is now 22.1*C, we turn off heater
+* Room goes below 22*C
+* Room goes below 21*C, we turn on the heater since we are below target minus threshold (22-1)*C
+* Room goes above 22*C, heater turns off
+* Repeat
---
+++ thermostat_and_ir.ino
@@ -0,0 +1,105 @@
+// Libraries
+#include <ACROBOTIC_SSD1306.h>
+#include <DHT.h>
+#include <IRremote.h>
+#include <Wire.h>
+
+// Constants
+#define DHTPIN 7      // what pin we're connected to
+#define DHTTYPE DHT22 // DHT 22  (AM2302)
+
+// My heater's remote control codes
+#define POWER 0x807F48B7
+#define UP_TEMP 0x807FB847
+#define DOWN_TEMP 0x807F9867
+
+// Global Variables
+DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
+float hum;                // Stores humidity value
+float temp;               // Stores temperature value
+
+IRsend irsend;
+bool heat_on = false;
+int target_temp = 22;
+int buffer_below = 1;
+bool post_heating = false;
+bool refresh = false;
+
+void setup()
+{
+    // Initialze SSD1306 OLED display
+    Wire.begin();
+    oled.init();
+    oled.clearDisplay();
+
+    // DHT22
+    dht.begin();
+}
+
+void update_screen(float temp, float hum)
+{
+    //  display
+    oled.setTextXY(0, 0);
+    oled.putString("Temp: " + String(temp) + "C");
+
+    oled.setTextXY(1, 0);
+    oled.putString("Humi: " + String(hum) + "%");
+    oled.setTextXY(2, 0);
+    oled.putString("Heat: " + String(heat_on) +
+                   ", Post: " + String(post_heating));
+
+    oled.setTextXY(3, 0);
+    if (refresh)
+    {
+        oled.putString(".");
+        refresh = false;
+    }
+    else
+    {
+        oled.putString(" ");
+        refresh = true;
+    }
+}
+
+void turn_on_heat()
+{
+    irsend.sendNEC(POWER, 32);
+    heat_on = true;
+}
+
+void turn_off_heat()
+{
+    irsend.sendNEC(POWER, 32);
+    heat_on = false;
+}
+
+void loop()
+{
+    // do 1 minute delay at the top so we have time to position device
+    delay(1000);
+
+    // Read data and store it to variables hum and temp
+    hum = dht.readHumidity();
+    temp = dht.readTemperature();
+
+    // Write status to screen
+    update_screen(temp, hum);
+
+    // if we are not waiting for buffering and the temp is low
+    if (temp < target_temp && !heat_on && !post_heating)
+    {
+        turn_on_heat();
+    }
+    // if the temp has reached the target temp
+    else if (temp >= target_temp && heat_on)
+    {
+        turn_off_heat();
+        post_heating = true;
+    }
+    // if we had been heating and are waiting for temp to go down
+    else if (temp < target_temp - buffer_below && post_heating)
+    {
+        turn_on_heat();
+        post_heating = false;
+    }
+}
